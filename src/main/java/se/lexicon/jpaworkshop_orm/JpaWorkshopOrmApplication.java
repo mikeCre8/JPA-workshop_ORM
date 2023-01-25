ef996947dd80b7dc363611d311ceb929175d9246
Mikael Svensson <113359307+mikeCre8@users.noreply.github.com>
2023-01-25 12:36:35 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/jpaworkshop_orm/JpaWorkshopOrmApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWorkshopOrmApplication {
+    
+    public static void main( String[] args ) {
+        SpringApplication.run(JpaWorkshopOrmApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/se/lexicon/jpaworkshop_orm/JpaWorkshopOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.jpaworkshop_orm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWorkshopOrmApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
